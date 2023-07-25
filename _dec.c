782bce0c2b01404a725f04f3edeb76e8cb40ab92
Zulaykha <suliat7@gmail.com>
2023-07-25 02:20:32 +0100

    Update Files

--- _dec.c
+++ _dec.c
@@ -9,32 +9,44 @@
 
 void _dec(va_list args, int *count)
 {
-	int dec;
+	int dec, i, j, num_len;
+	unsigned int num, pos_num;
+	char *digits;
 
 	dec = va_arg(args, int);
-	num = dec;
-	
+	if (dec < 0)
+		pos_num  = dec * -1;
+	else
+		pos_num = dec;
+	num = pos_num;
 	num_len = 0;
-	while (num_cpy / 10 != 0)
+	while (pos_num / 10 != 0)
 	{
 		num_len++;
-		num_cpy = num_cpy / 10;
+		pos_num = pos_num / 10;
 	}
 	num_len++;
-
 	digits = malloc(sizeof(char) * num_len + 1);
 	if (digits == NULL)
-		return ;
-	
+		return;
+
 	j = 0;
 	while (num / 10 != 0)
 	{
-
+		digits[j] = (num % 10) + '0';
+		num = num / 10;
+		j++;
 	}
-
-	for (i = 0; ; i++)
+	digits[j] = (num % 10) + '0';
+	if (dec < 0)
+	{
+		_putchar('-');
+		(*count)++;
+	}
+	for (i = j; i >= 0; i--)
 	{
 		_putchar(digits[i]);
 		(*count)++;
-	}	
+	}
+	free(digits);
 }
